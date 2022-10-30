b7290b0da9e77ec33ea35f120b52ffb1f202ba8e
onurkahyaoglu <onurkahyaoglu@gmail.com>
2022-10-30 22:19:13 +0300

    Hotelcontroller a tüm otelleri getiren, id ye göre otel search eden, name'e göre otel search edip ilk sonucu dönen ve name e göre otel search edip tüm sonuçları dönen methodlar eklendi.

--- HotelManagement.API/Controllers/HotelController.cs
+++ HotelManagement.API/Controllers/HotelController.cs
@@ -1,4 +1,5 @@
-﻿using HotelManagement.API.Models;
+﻿using HotelManagement.API.Component;
+using HotelManagement.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -8,6 +9,35 @@ namespace HotelManagement.API.Controllers
     [Route("api/[Controller]/[action]")]
     public class HotelController : ControllerBase
     {
+        [HttpGet]
+        public List<Hotel> GetAllHotels()
+        {
+            Components cmp = new Components();
+            List<Hotel> list = cmp.GetAllHotels();
+            return list;
+        }
 
+        [HttpGet("{id}")]
+        public Hotel HotelSearchById(int id)
+        {
+            Components cmp = new Components();
+            Hotel res = cmp.GetAllHotels().Where(_ => _.inId == id).FirstOrDefault();
+            return res;
+        }
+
+        [HttpGet("{name}")]
+        public Hotel HotelSearchByName(string name)
+        {
+            Components cmp = new Components();
+            Hotel res = cmp.GetAllHotels().Where(_ => _.stHotelName.Contains(name)).FirstOrDefault();
+            return res;
+        }
+        [HttpGet("{name}")]
+        public List<Hotel> HotelsSearchByName(string name)
+        {
+            Components cmp = new Components();
+            List<Hotel> res = cmp.GetAllHotels().Where(_ => _.stHotelName.Contains(name)).ToList();
+            return res;
+        }
     }
 }
