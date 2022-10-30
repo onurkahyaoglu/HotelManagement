dc7e6763ffb32e3595d5de6d38bbb1290459e533
onurkahyaoglu <onurkahyaoglu@gmail.com>
2022-10-30 22:09:28 +0300

    HotelController eklendi

---
+++ HotelManagement.API/Controllers/HotelController.cs
@@ -0,0 +1,13 @@
+﻿using HotelManagement.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HotelManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]/[action]")]
+    public class HotelController : ControllerBase
+    {
+
+    }
+}
