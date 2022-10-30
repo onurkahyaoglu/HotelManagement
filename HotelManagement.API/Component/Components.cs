aab85be5b9623b2e51b3af939457e199e44f7c9d
onurkahyaoglu <onurkahyaoglu@gmail.com>
2022-10-30 22:11:02 +0300

    Components sınıfı eklendi,  otel listesi döndürüldü

---
+++ HotelManagement.API/Component/Components.cs
@@ -0,0 +1,15 @@
+﻿using HotelManagement.API.Fake;
+using HotelManagement.API.Models;
+
+namespace HotelManagement.API.Component
+{
+    public class Components
+    {
+        //Otel listesini döner
+        public List<Hotel> GetAllHotels()
+        {
+            List<Hotel> list = FakeHotelData.GetHotels(10);
+            return list;
+        }
+    }
+}
